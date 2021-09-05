4576b3c740aac8a59d440b2d7b974d8bb99ab6f8
roman-uk <roman55@mail.com>
2021-09-05 15:10:25 +0200

    modified:.gitignore (config_email.py)

---
+++ photographer/config_email.py
@@ -0,0 +1,14 @@
+login = 'oliwia_foto@rambler.ru'
+password = 'Foto_oliwia1'
+
+
+# h: oliwia 	p: test5555
+
+
+
+# Access Key ID:    AKIAVXELWD6P2FFGOSND
+# Secret Access Key:    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
+
+# database
+	# Master username: oliwa
+	# password: Foto_oliwia1 
