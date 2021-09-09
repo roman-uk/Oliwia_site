c54d19a713528d9c3b67227814298c21511670ce
roman-uk <roman55@mail.com>
2021-09-09 19:06:51 +0200

    Add cloudinary for immage

--- Oliwia_site/settings.py
+++ Oliwia_site/settings.py
@@ -42,7 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'photographer'
+    'cloudinary_storage',
+    'cloudinary',
+    'photographer',
 ]
 
 MIDDLEWARE = [
@@ -130,8 +132,15 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 django_heroku.settings(locals())
 
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': 'dtabjdkaf',
+    'API_KEY': '494236315171716',
+    'API_SECRET': 'V2nqsRLlbAsht3iEz0-Fo_AU5q4'
+}
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
